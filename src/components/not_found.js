78a37093eb328c8433efab30cbfce0e0ec88e96f
LilianKalkman <liliankalkman@hotmail.com>
2018-02-19 15:10:25 +0100

    eventhandler

--- src/components/not_found.js
+++ src/components/not_found.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const NotFound = () => {
   return (
     <h3>Not Found!</h3>
--- src/components/storepicker.js
+++ src/components/storepicker.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  goToStore(event){
+    event.preventDefault();
+    console.log(this.StoreInput.value);
+
+  }
+
   render(){
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input
+          type="text" required placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={(input) => {this.StoreInput = input}} />
         <button type="submit">Visit Store -></button>
       </form>
     );
