ca144c9c6d22c9cd8dd87396119c99cf2eb6eafa
LilianKalkman <liliankalkman@hotmail.com>
2018-02-19 13:14:05 +0100

    routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
 import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/storepicker';
+import NotFound from './components/not_found';
 
-ReactDom.render(<StorePicker />, document.querySelector('#main'));
-ReactDom.render(<App />, document.querySelector('#main'));
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+
+ReactDom.render(<Root />, document.querySelector('#main'));
