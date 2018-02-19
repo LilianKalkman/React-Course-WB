64ec01c75fb0b81af79982fa4fb8446f89a730ae
LilianKalkman <liliankalkman@hotmail.com>
2018-02-19 12:30:42 +0100

    header

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Header from './header';
+import Order from './order';
+import Inventory from './inventory';
+
+class App extends Component {
+  render(){
+    return(
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import './css/style.css';
+import App from './components/App';
 import StorePicker from './components/storepicker';
 
 ReactDom.render(<StorePicker />, document.querySelector('#main'));
+ReactDom.render(<App />, document.querySelector('#main'));
